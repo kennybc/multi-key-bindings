a5afe02f4320ea1811b39cbbca1c37a3ae1edf89
kennybc <kennybc@users.noreply.github.com>
2025-12-27 17:03:20 -0800

    fix import path

--- src/client/java/us/kenny/mixin/KeyBindsScreenMixin.java
+++ src/client/java/us/kenny/mixin/KeyBindsScreenMixin.java
@@ -1,11 +1,12 @@
 package us.kenny.mixin;
 
 import com.mojang.blaze3d.platform.InputConstants;
-import net.minecraft.Util;
 import net.minecraft.client.gui.screens.options.controls.KeyBindsList;
 import net.minecraft.client.gui.screens.options.controls.KeyBindsScreen;
 import net.minecraft.client.input.KeyEvent;
 import net.minecraft.client.input.MouseButtonEvent;
+import net.minecraft.util.Util;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;

bbc3c4809c1147a37f2efde13a76a5b99ccfa554
kennybc <kennybc@users.noreply.github.com>
2026-05-03 17:10:25 -0700

    Version 1.3.0

--- src/client/java/us/kenny/mixin/KeyBindsScreenMixin.java
+++ src/client/java/us/kenny/mixin/KeyBindsScreenMixin.java
@@ -1,20 +1,23 @@
 package us.kenny.mixin;
 
-import com.mojang.blaze3d.platform.InputConstants;
-
-import net.minecraft.Util;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.client.gui.screens.options.controls.KeyBindsList;
 import net.minecraft.client.gui.screens.options.controls.KeyBindsScreen;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.mojang.blaze3d.platform.InputConstants;
+
 import us.kenny.MultiKeyBindingManager;
 import us.kenny.core.MultiKeyBinding;
 import us.kenny.core.MultiKeyBindingScreen;
+import us.kenny.core.MultiKeyBindingScreenHelper;
 
 @Mixin(KeyBindsScreen.class)
 public abstract class KeyBindsScreenMixin implements MultiKeyBindingScreen {
@@ -22,9 +25,17 @@ public abstract class KeyBindsScreenMixin implements MultiKeyBindingScreen {
     public long lastKeySelection;
     @Shadow
     private KeyBindsList keyBindsList;
+    @Mutable
+    @Shadow
+    private KeyMapping selectedKey;
 
     @Unique
     private MultiKeyBinding selectedMultiKeyBinding;
+
+    @Unique
+    public KeyMapping getSelectedKey() {
+        return this.selectedKey;
+    }
 
     @Unique
     public MultiKeyBinding getSelectedMultiKeyBinding() {
@@ -32,8 +43,18 @@ public abstract class KeyBindsScreenMixin implements MultiKeyBindingScreen {
     }
 
     @Unique
+    public void setSelectedKey(KeyMapping keyMapping) {
+        this.selectedKey = keyMapping;
+    }
+
+    @Unique
     public void setSelectedMultiKeyBinding(MultiKeyBinding multiKeyBinding) {
         this.selectedMultiKeyBinding = multiKeyBinding;
+    }
+
+    @Unique
+    public void setLastKeySelection(long time) {
+        this.lastKeySelection = time;
     }
 
     /**
@@ -41,14 +62,14 @@ public abstract class KeyBindsScreenMixin implements MultiKeyBindingScreen {
      * Updates selected custom key binding with whatever mouse button was pressed.
      */
     @Inject(method = "mouseClicked", at = @At("HEAD"), cancellable = true)
-    private void onMouseClicked(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> cir) {
+    private void onMouseClicked(double mouseX, double mouseY, int button,
+            CallbackInfoReturnable<Boolean> cir) {
         if (this.selectedMultiKeyBinding != null) {
             MultiKeyBindingManager.setKeyBinding(this.selectedMultiKeyBinding,
                     InputConstants.Type.MOUSE.getOrCreate(button));
             this.selectedMultiKeyBinding = null;
             this.keyBindsList.resetMappingAndUpdateButtons();
             cir.setReturnValue(true);
-            cir.cancel();
         }
     }
 
@@ -58,20 +79,9 @@ public abstract class KeyBindsScreenMixin implements MultiKeyBindingScreen {
      */
     @Inject(method = "keyPressed", at = @At("HEAD"), cancellable = true)
     public void onKeyPressed(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
-        if (this.selectedMultiKeyBinding != null) {
-            if (keyCode == InputConstants.KEY_ESCAPE) {
-                MultiKeyBindingManager.setKeyBinding(this.selectedMultiKeyBinding,
-                        InputConstants.UNKNOWN);
-            } else {
-                MultiKeyBindingManager.setKeyBinding(this.selectedMultiKeyBinding,
-                        InputConstants.getKey(keyCode, scanCode));
-            }
-
-            this.selectedMultiKeyBinding = null;
-            this.lastKeySelection = Util.getMillis();
-            this.keyBindsList.resetMappingAndUpdateButtons();
+        if (MultiKeyBindingScreenHelper.handleKeyPressed(this, this.keyBindsList,
+                InputConstants.getKey(keyCode, scanCode))) {
             cir.setReturnValue(true);
-            cir.cancel();
         }
     }
 }

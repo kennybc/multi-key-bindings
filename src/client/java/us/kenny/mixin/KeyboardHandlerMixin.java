b0659df6195a7d31f37d1c19442724f8cf9489fe
kennybc <kennybc@users.noreply.github.com>
2026-05-03 17:21:03 -0700

    Version 1.3.0

---
+++ src/client/java/us/kenny/mixin/KeyboardHandlerMixin.java
@@ -0,0 +1,38 @@
+package us.kenny.mixin;
+
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.Util;
+import net.minecraft.client.KeyboardHandler;
+import net.minecraft.client.Minecraft;
+import us.kenny.core.MultiKeyBindingScreen;
+
+@Mixin(KeyboardHandler.class)
+public class KeyboardHandlerMixin {
+
+    /**
+     * Injected in the method keyPress:
+     * Clears selected key binding so we can stop tracking modifier key events in
+     * the controls screen.
+     */
+    @Inject(method = "keyPress", at = @At(value = "FIELD", target = "Lnet/minecraft/client/KeyboardHandler;debugCrashKeyTime:J", ordinal = 0, opcode = Opcodes.GETFIELD))
+    private void onKeyPress(
+            long window,
+            int key,
+            int scancode,
+            int action,
+            int modifiers,
+            CallbackInfo callbackInfo) {
+        if (action == 0 && Minecraft.getInstance().screen instanceof MultiKeyBindingScreen screen) {
+            if (screen.getSelectedKey() != null || screen.getSelectedMultiKeyBinding() != null) {
+                screen.setSelectedKey(null);
+                screen.setSelectedMultiKeyBinding(null);
+                screen.setLastKeySelection(Util.getMillis());
+            }
+        }
+    }
+}
